e9b2727d353e2ac278a38c5eb9a7e92400920fa0
Bryan Reamillo <bryanreamillo2403@gmail.com>
2026-03-29 22:48:11 +0800

    Move getSuccessor function to removeDupe.js and ensure proper formatting

--- src/lib/removeDupe.js
+++ src/lib/removeDupe.js
@@ -2,3 +2,13 @@ export function removeDuplicate(arr) {
     const set = new Set(arr);
     return [...set];
 }
+
+export function getSuccessor(node) {
+    node = node.rightChild
+
+    while (node && node.leftChild) {
+        node = node.leftChild;
+    }
+
+    return node;
+} 
